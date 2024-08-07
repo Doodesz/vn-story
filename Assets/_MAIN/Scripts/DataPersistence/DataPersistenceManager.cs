07d758a2ccd851e3f9e6ce195640c2f790bb97fc
Doodesz <158542315+Doodesz@users.noreply.github.com>
2024-08-07 13:13:35 +0700

    Fixed new game then load game bug

--- Assets/_MAIN/Scripts/DataPersistence/DataPersistenceManager.cs
+++ Assets/_MAIN/Scripts/DataPersistence/DataPersistenceManager.cs
@@ -60,7 +60,6 @@ public class DataPersistenceManager : MonoBehaviour
     public void NewGame() 
     {
         this.gameData = new GameData();
-        SaveGame();
     }
 
     public void LoadGame()
